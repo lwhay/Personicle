c72a7169fae655d92358961fe03272e99e66559b
lwhay <lwh@whu.edu.cn>
2019-10-06 00:29:07 -0700

    revert cxf down to lower verion-2.2.3.

--- src/main/java/personicle/webservice/GlobalUUIDCXFService.java
+++ src/main/java/personicle/webservice/GlobalUUIDCXFService.java
@@ -1,13 +1,16 @@
 package personicle.webservice;
 
+import java.net.SocketException;
+
 import com.fasterxml.uuid.EthernetAddress;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedGenerator;
 import org.apache.cxf.frontend.ServerFactoryBean;
-
-import javax.xml.ws.Endpoint;
+import org.apache.log4j.Logger;
+import personicle.utils.IPAddressUtil;
 
 public class GlobalUUIDCXFService {
+    private static Logger LOGGER = Logger.getLogger(GlobalUUIDCXFService.class);
 
     TimeBasedGenerator nbg = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
 
@@ -15,11 +18,17 @@ public class GlobalUUIDCXFService {
         return nbg.generate().toString().replace("-", "").trim();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
         ServerFactoryBean serverFactoryBean = new ServerFactoryBean();
-        String address = "http://192.168.56.133:10901/GlobalUUIDSerivce";
-        serverFactoryBean.setAddress(address);
-        serverFactoryBean.setServiceClass(GlobalUUIDService.class);
-        serverFactoryBean.create();
+        String ipAddress = IPAddressUtil.getEffectiveLocalIPAddress();
+        if (ipAddress != null) {
+            System.out.println(ipAddress);
+            String address = "http://" + ipAddress + ":10901/GlobalUUIDCXFService";
+            serverFactoryBean.setAddress(address);
+            serverFactoryBean.setServiceClass(GlobalUUIDService.class);
+            serverFactoryBean.create();
+        } else {
+            LOGGER.error("Ip address not found!");
+        }
     }
 }
--- src/main/java/personicle/webservice/GlobalUUIDService.java
+++ src/main/java/personicle/webservice/GlobalUUIDService.java
@@ -3,12 +3,17 @@ package personicle.webservice;
 import com.fasterxml.uuid.EthernetAddress;
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedGenerator;
+import personicle.utils.IPAddressUtil;
 
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
+import java.net.SocketException;
+
+import org.apache.log4j.Logger;
 
 @WebService
 public class GlobalUUIDService {
+    private static Logger LOGGER = Logger.getLogger(GlobalUUIDService.class);
 
     TimeBasedGenerator nbg = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
 
@@ -16,8 +21,22 @@ public class GlobalUUIDService {
         return nbg.generate().toString().replace("-", "").trim();
     }
 
-    public static void main(String[] args) {
-        String address = "http://192.168.56.133:10901/GlobalUUIDSerivce";
-        Endpoint.publish(address, new GlobalUUIDService());
+    public String getReturn() {
+        return nbg.generate().toString().replace("-", "").trim();
+    }
+
+    public String setReturn() {
+        return nbg.generate().toString().replace("-", "").trim();
+    }
+
+    public static void main(String[] args) throws SocketException {
+        String ipAddress = IPAddressUtil.getEffectiveLocalIPAddress();
+        if (ipAddress != null) {
+            System.out.println(ipAddress);
+            String address = "http://" + ipAddress + ":10901/GlobalUUIDSerivce";
+            Endpoint.publish(address, new GlobalUUIDService());
+        } else {
+            LOGGER.error("IP not found!");
+        }
     }
 }
