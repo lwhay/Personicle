f02f217c6437dfd742f804d5121770f484ae817c
Wenhai Li <lwh@whu.edu.cn>
2020-12-04 01:47:20 +0800

    revise to server

--- src/test/java/mongo/BigFoodTest.java
+++ src/test/java/mongo/BigFoodTest.java
@@ -11,20 +11,22 @@ import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.gt;
 import static com.mongodb.client.model.Filters.lt;
 
 public class BigFoodTest {
-    public static void main(String[] args) {
+    private static void filterQuery() {
         try (MongoClient mongoClient = MongoClients.create("mongodb://172.16.2.225:27017")) {
 
-            MongoDatabase database = mongoClient.getDatabase("Personicle");
+            MongoDatabase database = mongoClient.getDatabase("PersonicleServer");
 
-            MongoCollection<Document> collection = database.getCollection("cars");
+            MongoCollection<Document> collection = database.getCollection("foodlog");
 
-            FindIterable fit = collection.find(and(lt("price", 50000),
+            FindIterable fit = collection.find(and(lt("id", 50000),
                     gt("price", 20000))).sort(new Document("price", -1));
 
             List<Document> docs = new ArrayList<Document>();
@@ -36,5 +38,23 @@ public class BigFoodTest {
                 System.out.println(doc);
             }
         }
+    }
+
+    private static void listQuery() {
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+
+        try (MongoClient mongoClient = MongoClients.create("mongodb://172.16.2.225:27017")) {
+
+            MongoDatabase database = mongoClient.getDatabase("PersonicleServer");
+
+            for (String name : database.listCollectionNames()) {
+                System.out.println(name);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        listQuery();
     }
 }
