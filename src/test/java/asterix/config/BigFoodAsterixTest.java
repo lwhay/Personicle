e3d62dcbcf66da3bbb43af7fa8ddc2c22cb67018
Wenhai Li <lwh@whu.edu.cn>
2020-12-04 23:23:06 +0800

    add batching model for asterix

--- src/test/java/asterix/config/BigFoodAsterixTest.java
+++ src/test/java/asterix/config/BigFoodAsterixTest.java
@@ -7,19 +7,57 @@ import personicle.query.JSONQuery;
 import java.util.List;
 
 public class BigFoodAsterixTest {
-    public static void main(String[] args) throws Exception {
-        List<JSONObject> queries = JSONQuery.getQuery();
-        AsterixConf conf = new AsterixConf("http://172.16.2.225:19002");
-        AsterixConn conn = new AsterixConn();
-        conf.setDataverse("PersonicleServer");
-        int count = 0;
-        long begin = System.currentTimeMillis();
-        for (JSONObject query : queries) {
-            String q = "SELECT * FROM FoodLog WHERE `userName` = '" + query.get("userName") + "';";
-            conf.setBody(q);
-            String ret = conn.handleRequest(conf, AsterixConf.OpType.QUERY);
-            count += ((JSONArray) (JSONObject.parseObject(ret).get("results"))).size();
+    private static void rrQuery() {
+        try {
+            List<JSONObject> queries = JSONQuery.getQuery();
+            AsterixConf conf = new AsterixConf("http://172.16.2.225:19002");
+            AsterixConn conn = new AsterixConn();
+            conf.setDataverse("PersonicleServer");
+            int count = 0;
+            long begin = System.currentTimeMillis();
+            for (JSONObject query : queries) {
+                String q = "SELECT * FROM FoodLog WHERE `userName` = '" + query.get("userName") + "';";
+                conf.setBody(q);
+                String ret = conn.handleRequest(conf, AsterixConf.OpType.QUERY);
+                count += ((JSONArray) (JSONObject.parseObject(ret).get("results"))).size();
+            }
+            System.out.println(count + " " + queries.size() + " " + (System.currentTimeMillis() - begin));
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        System.out.println(count + " " + queries.size() + " " + (System.currentTimeMillis() - begin));
+    }
+
+    private static void batchQueray(int batch_size) {
+        try {
+            List<JSONObject> queries = JSONQuery.getQuery();
+            AsterixConf conf = new AsterixConf("http://172.16.2.225:19002");
+            AsterixConn conn = new AsterixConn();
+            conf.setDataverse("PersonicleServer");
+            int count = 0, tick = 0;
+            String q = "SELECT * FROM FoodLog WHERE `userName` in [";
+            long begin = System.currentTimeMillis();
+            for (JSONObject query : queries) {
+                q += "'" + query.get("userName") + "'";
+                if (++tick % batch_size == 0) {
+                    q += "];";
+                    conf.setBody(q);
+                    String ret = conn.handleRequest(conf, AsterixConf.OpType.QUERY);
+                    count += ((JSONArray) (JSONObject.parseObject(ret).get("results"))).size();
+                    q = "SELECT * FROM FoodLog WHERE `userName` in [";
+                } else {
+                    q += ",";
+                }
+            }
+            System.out.println(count + " " + queries.size() + " " + (System.currentTimeMillis() - begin));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // rrQuery();
+        if (true) {
+            batchQueray(Integer.parseInt(args[0]));
+        }
     }
 }
